c7f3c24d909716dad2a36bd50ef5f57c8df47253
Jessica Ayala <jessicaayala@Jessicas-MBP.lan>
2020-08-26 14:48:59 -0700

    made small clean up with spacing

--- feature-step-definitions/createAccount.py
+++ feature-step-definitions/createAccount.py
@@ -2,6 +2,7 @@ from selenium import webdriver
 from behave import *
 from selenium.webdriver.common.action_chains import ActionChains
 from selenium.webdriver.common.keys import Keys
+from selenium.common.exceptions import NoSuchElementException
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as cond
 from selenium.common.exceptions import NoAlertPresentException
@@ -12,10 +13,7 @@ from time import sleep
 # def i_am_on_the_amazon_homepage():
 driver = webdriver.Chrome("/usr/local/bin/chromedriver")
 driver.get("https://www.Amazon.com")
-    # home_logo = driver.find_elements_by_css_selector(".nav-logo-link")
-    # print(driver.title)
-    # driver.close()
-#
+
 # @And ('^I click on the link')
 # def i_click_on_the_create_account_link():
 action = ActionChains(driver)
@@ -24,13 +22,11 @@ action.move_to_element(sign_in_menu).perform()
 sign_in_sub_menu = driver.find_element_by_css_selector(".nav_pop_new_cust > a")
 sign_in_sub_menu.click()
 
-
-# @And ('^I am on the \* page')
+# @And ('^I am on the (\d+) page')
 # def i_am_on_the_create_account_page():
 driver.implicitly_wait(4)
 create_account_module_title = driver.find_element_by_css_selector("h1.a-spacing-small")
 
-#
 # @And ('^I enter valid user information')
 # def i_enter_valid_user_information():
 name_field = driver.find_element_by_css_selector("#ap_customer_name")
@@ -39,7 +35,6 @@ name_field.send_keys("Flor Ayala")
 # print("Opened email generator in new tab")
 # driver.implicitly_wait(10)
 # copy_button = driver.find_element_by_css_selector("#click-to-copy")
-# driver.close()
 email_field = driver.find_element_by_css_selector("#ap_email")
 email_field.send_keys("rofofon813@synevde.com")
 email_field.send_keys(Keys.TAB)
@@ -50,14 +45,8 @@ re_enter_pwd_field = driver.find_element_by_css_selector("#ap_password_check")
 re_enter_pwd_field.send_keys("testing0123")
 re_enter_pwd_field.send_keys(Keys.TAB)
 
-
 # @When ('^I click on Create Your Amazon Account')
 # def i_click_on_create_your_amazon_account_button():
 create_account_button = driver.find_element_by_css_selector("#continue")
 create_account_button.click()
-
-
-
-# @Then ('^I am on the confirm identity security page')
-# def i_am_on_the_confirm_identity_security_page():
-
+driver.quit()
