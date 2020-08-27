4d6f32aa8851468f05e54cbed1b68bb2cdee219e
Jessica Ayala <jessicaayala@Jessicas-MBP.lan>
2020-08-27 08:04:31 -0700

    comments per step def

--- feature-step-definitions/addingItems.py
+++ feature-step-definitions/addingItems.py
@@ -2,37 +2,37 @@ from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 from time import sleep
 from behave import *
-#
+
+# This scenario launches Amazon homepage
 # @Given('^I am on the Amazon homepage')
 # def i_am_on_the_amazon_homepage():
 driver = webdriver.Chrome("/usr/local/bin/chromedriver")
 driver.get("https://www.Amazon.com")
 sleep(2)
 
-#
+# This function uses send keys to search for a specified item
 # @And('^I search for any item')
 # def i_search_for_any_item():
 search_text_box = driver.find_element_by_css_selector("#twotabsearchtextbox")
-sleep(1)
 search_text_box.send_keys("coffee")
 search_text_box.send_keys(Keys.ENTER)
-# search_submit_button = driver.find_element_by_css_selector("#nav-search span.nav-sprite")
 
-
+# Driver searches for item with given css selector and clicks on it
 # @And('^I add to my cart')
 # def i_add_to_my_cart():
 item = driver.find_element_by_css_selector("div#search div:nth-child(3) > div > span div:nth-child(3) a").click()
-sleep(2)
-cart_button = driver.find_element_by_css_selector("#a-autoid-20 > span").click()
+cart_button = driver.find_element_by_css_selector("#a-autoid-20 > span")
+cart_button.click()
 
-
+# Driver navigates to cart
 # @When('^I navigate to my cart')
 # def i_navigate_to_my_cart():
 sleep(3)
-nav_cart = driver.find_element_by_css_selector("a#nav-cart").click()
+nav_cart = driver.find_element_by_css_selector("a#nav-cart")
+nav_cart.click()
 
-
-# @Then('^I see the correct item in my car')
+# Used if else statement to validate if item in cart is correct
+# @Then('^I see the correct item in my cart')
 # def i_see_the_correct_item_in_my_car():
 item_validation= "AmazonFresh Colombia Ground Coffee, Medium Roast, 12 Ounce"
 if item_validation!=item_validation:
--- feature-step-definitions/createAccount.py
+++ feature-step-definitions/createAccount.py
@@ -9,11 +9,13 @@ from selenium.common.exceptions import NoAlertPresentException
 from selenium.common.exceptions import TimeoutException
 from time import sleep
 
+# This scenario launches Amazon homepage
 # @Given('^I am on the Amazon homepage')
 # def i_am_on_the_amazon_homepage():
 driver = webdriver.Chrome("/usr/local/bin/chromedriver")
 driver.get("https://www.Amazon.com")
 
+# This scenario takes user to click on the sign up option
 # @And ('^I click on the link')
 # def i_click_on_the_create_account_link():
 action = ActionChains(driver)
@@ -22,11 +24,13 @@ action.move_to_element(sign_in_menu).perform()
 sign_in_sub_menu = driver.find_element_by_css_selector(".nav_pop_new_cust > a")
 sign_in_sub_menu.click()
 
+# A variable for the create account module is created to find the specified element selector
 # @And ('^I am on the (\d+) page')
 # def i_am_on_the_create_account_page():
 driver.implicitly_wait(4)
 create_account_module_title = driver.find_element_by_css_selector("h1.a-spacing-small")
 
+# Send keys function is used to fill in user information into each field specified
 # @And ('^I enter valid user information')
 # def i_enter_valid_user_information():
 name_field = driver.find_element_by_css_selector("#ap_customer_name")
@@ -45,6 +49,7 @@ re_enter_pwd_field = driver.find_element_by_css_selector("#ap_password_check")
 re_enter_pwd_field.send_keys("testing0123")
 re_enter_pwd_field.send_keys(Keys.TAB)
 
+# A variable is created for the Create Your Account button for the driver to click on upon specifying the element selector
 # @When ('^I click on Create Your Amazon Account')
 # def i_click_on_create_your_amazon_account_button():
 create_account_button = driver.find_element_by_css_selector("#continue")
